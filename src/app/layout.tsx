dc7b2f2f324c4bbbfd6b1002d934e1d9c3200da9
alfanalivan <alfanalivan@gmail.com>
2023-10-31 13:07:47 +0700

    fix: internationalize

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="id">
+    <html lang="en">
       <body className={fontSans.className}>{children}</body>
     </html>
   );
